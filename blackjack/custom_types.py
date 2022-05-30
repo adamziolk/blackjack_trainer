9147b96234de70c184b993282d2e1fa201ab8552
adamziolk <adam.lawrence.ziolkowski@gmail.com>
2022-05-30 16:48:47 -0600

    Revert "Revert "Overthinking - reverting to original""
    This reverts commit 38e9863532d743aedac046db7e96d0bf0f79039b.

--- blackjack/custom_types.py
+++ blackjack/custom_types.py
@@ -3,6 +3,17 @@ class Card:
         self.value = value
         self.suit = suit
 
+    def __repr__(self):
+        return f':{self.value}, {self.suit}:'
+
+    def __eq__(self, other):
+        if isinstance(self, other.__class__):
+            return self.value == other.value and self.suit == other.suit
+        return False
+
+    def __hash__(self):
+        return hash(f'{self.value} {self.suit}')
+
 
 class Shoe:
     pass
@@ -14,8 +25,30 @@ class Player:
         self.id = id
         self.cards = []
 
+    def __repr__(self):
+        return f'{self.id} - {self.cards}'
+
+    def __eq__(self, other):
+        if isinstance(self, other.__class__):
+            return self.id == other.id and self.cards == other.cards
+        return False
+
+    def __hash__(self):
+        return hash(f'{self.id} {self.cards}')
+
 
 class Table:
     def __init__(self, players):
         self.dealer_cards = []
         self.players = players
+
+    def __repr__(self):
+        return f'{[p.id for p in self.players]}'
+
+    def __eq__(self, other):
+        if isinstance(self, other.__class__):
+            return self.dealer_cards == other.dealer_cards and self.players == other.players
+        return False
+
+    def __hash__(self):
+        return hash(f'{self.dealer_cards} {self.players}')
